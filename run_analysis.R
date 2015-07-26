3446c5149a5193b1dc2157d455ad91eb1eecdf87
hongthaikoh <hongthai.koh@gmail.com>
2015-07-26 22:18:16 +0800

    this update is to submit course project for getting and cleanning data

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,12 @@
-#This project is to prepare tidy data based on UCI HAR dataset
-#the steps are:
-#1. Merges the training and the test sets to create one data set.
-#2. Extracts only the measurements on the mean and standard deviation for each measurement.
-#3. Uses descriptive activity names to name the activities in the data set
-#4. Appropriately labels the data set with descriptive variable names.
-#5. From the data set in step 4, creates a second, independent tidy data set
-#   with the average of each variable for each activity and each subject.
+# Author: Hong Thai Koh (26 July 2015) 
+# This project is to prepare tidy data based on UCI HAR dataset
+# The steps to follow are:
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names.
+# 5. From the data set in step 4, creates a second, independent tidy data set
+#    with the average of each variable for each activity and each subject.
 
 # load dplyr package
 library(dplyr)
